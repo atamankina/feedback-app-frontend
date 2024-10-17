16852ccc09b5178d750690bc57ce16c4378c7bdf
Galina Atamankina <g.atamankina@gmail.com>
2024-10-17 04:58:30 +0200

    Remove cors anywhere.

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const BACKEND_URL = `/feedback`;
-export const PREDICTION_API_URL = 'http://localhost:8080/https://tx6egorbv3.execute-api.eu-central-1.amazonaws.com/dev';
+export const PREDICTION_API_URL = 'https://tx6egorbv3.execute-api.eu-central-1.amazonaws.com/dev';
