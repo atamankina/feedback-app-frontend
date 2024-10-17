2c2b21cff9e9e00f31fa3f91d742353981b2f39d
Galina Atamankina <g.atamankina@gmail.com>
2024-10-17 04:53:38 +0200

    Using proxy server for cors.

--- src/api/feedbackApi.js
+++ src/api/feedbackApi.js
@@ -1,27 +1,64 @@
-import { BACKEND_URL } from '../config';
+import { PREDICTION_API_URL } from '../config';
 
-export const createFeedback = async (feedback) => {
-    const response = await fetch(BACKEND_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(feedback)
+// Create a new prediction
+export const createPrediction = async (question) => {
+  try {
+    const response = await fetch(`${PREDICTION_API_URL}/prediction`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ question }),
     });
-    const result = await response.json();
-    return result.data;
-}
 
-export const getFeedback = async () => {
-    const response = await fetch(BACKEND_URL);
-    const result = await response.json();
-    return result.data;
-}
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
 
-export const deleteFeedback = async (title) => {
-    const response = await fetch(`${BACKEND_URL}/${title}`, {
-        method: 'DELETE'
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error('Error creating prediction:', error);
+  }
+};
+
+// Get all predictions from the API
+export const getPredictions = async () => {
+  try {
+    const response = await fetch(`${PREDICTION_API_URL}/prediction`, {
+      method: 'GET',
     });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result.map(prediction => ({
+      question: prediction.question,
+      predictionId: prediction.predictionId,
+      prediction: prediction.prediction,
+    }));
+  } catch (error) {
+    console.error('Error fetching predictions:', error);
+    return [];
+  }
+};
+
+// Delete a specific prediction by ID
+export const deletePrediction = async (id) => {
+  try {
+    const response = await fetch(`${PREDICTION_API_URL}/prediction/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
     const result = await response.json();
     return result.message;
-}
+  } catch (error) {
+    console.error(`Error deleting prediction with ID ${id}:`, error);
+  }
+};
